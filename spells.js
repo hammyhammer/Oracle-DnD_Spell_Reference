04bb5695a63504172928f9a4c5f3e8af72b3ec22
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2021-12-13 15:07:51 -0500

    added display data

--- spells.js
+++ spells.js
@@ -22,16 +22,39 @@ function showSpellData(data) {
   const spellName = document.createElement("p");
   spellName.innerText = `${data.name}`;
   divLeftPage.appendChild(spellName);
-}
 
-//Showing Casting time
 
-//Showinng Range
+  //Showing Casting time
+  const castingTime = document.createElement("p");
+  castingTime.innerText = `Casting Time: ${data.casting_time}`;
+  divLeftPage.appendChild(castingTime);
 
-//Showing Duration
+  //Showing Range
+  const range = document.createElement("p");
+  range.innerText = `Range:${data.range}`;
+  divLeftPage.appendChild(range);
 
-//Showing Concentration
+  //Showing Duration
+  const duration = document.createElement("p");
+  duration.innerText = `Duration:${data.duration}`
+  divLeftPage.appendChild(duration);
 
-//Showing Saving Throw Info
+  //Showing Concentration
+  const concentration = document.createElement("p");
+  concentration.innerText = `Concentration:${data.concentration}`;
+  divLeftPage.appendChild(concentration);
 
-//Showing Damage per Spell Slot
+  //Showing Saving Throw Info
+  const savingThrow = document.createElement("p")
+  savingThrow.innerText = `Saving Throw: ${data.dc.dc_type.name}`
+  divLeftPage.appendChild(savingThrow);
+
+  //Showing Damage Type
+  const damageType = document.createElement("p");
+  damage.innerText = `Damage Type:${data.damage.damage_type}`
+
+  //Showing Damage per Spell Slot
+  const damage = document.createElement("p");
+  damage.innerText = `Damage: ${data.damage.damage_at_slot_level}`;
+  divRightPage.appendChild(damage)
+} 
