ad409ae411b8300dbfc89264cbe4536797b00ec9
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2021-12-14 11:14:18 -0500

    added sections to book div

--- spells.js
+++ spells.js
@@ -6,13 +6,14 @@ const blankSpace = document.querySelector("#blank-space");
 async function fetchData(spell) {
   const url = `https://www.dnd5eapi.co/api/spells/${spell}`;
   const response = await axios.get(url);
-  // console.log(response);
+  console.log(response);
   const spellData = response.data;
 
   console.log(spellData)
-  Object.keys(showSpellData).forEach(function (data) {
-    console.log(data.name)
-    // showSpellData(spellObj)
+  Object.keys(showSpellData).forEach(function (spellObj) {
+    // console.log(data.name)
+    showSpellData(spellObj)
+    // console.log(spellData)
   });
   // const gettingSpellData = response.data.data.results
   // fetchData("fireball");
@@ -23,42 +24,42 @@ async function fetchData(spell) {
 //Showing the Spell information
 //Showing the name of spell
 function showSpellData(data) {
-  const spellName = document.createElement("h4");
+  const spellName = document.createElement("h2");
   spellName.innerText = `${data.name}`;
   divLeftPage.appendChild(spellName);
 
   //Showing Casting time
-  const castingTime = document.createElement("p");
+  const castingTime = document.createElement("h2");
   castingTime.innerText = `Casting Time: ${data.casting_time}`;
   divLeftPage.appendChild(castingTime);
 
   //Showing Range
-  const range = document.createElement("p");
+  const range = document.createElement("h2");
   range.innerText = `Range:${data.range}`;
   divLeftPage.appendChild(range);
 
   //Showing Duration
-  const duration = document.createElement("p");
+  const duration = document.createElement("h2");
   duration.innerText = `Duration:${data.duration}`
   divLeftPage.appendChild(duration);
 
   //Showing Concentration
-  const concentration = document.createElement("p");
+  const concentration = document.createElement("h2");
   concentration.innerText = `Concentration:${data.concentration}`;
   divLeftPage.appendChild(concentration);
 
   //Showing Saving Throw Info
-  const savingThrow = document.createElement("p")
+  const savingThrow = document.createElement("h2")
   savingThrow.innerText = `Saving Throw: ${data.dc.dc_type.name}`
   divLeftPage.appendChild(savingThrow);
 
   //Showing Damage Type
-  const damageType = document.createElement("p");
+  const damageType = document.createElement("h2");
   damage.innerText = `Damage Type:${data.damage.damage_type}`
   divRightPage.appendChild(damageType);
 
   //Showing Damage per Spell Slot
-  const damage = document.createElement("p");
+  const damage = document.createElement("h2");
   damage.innerText = `Damage: ${data.damage.damage_at_slot_level}`;
   divRightPage.appendChild(damage)
 }
