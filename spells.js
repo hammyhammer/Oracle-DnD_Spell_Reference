b6d87a1ee2dee1d6e123c2d94635b39b53cfe499
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2021-12-14 15:59:12 -0500

    cleaning up js

--- spells.js
+++ spells.js
@@ -13,15 +13,14 @@ async function fetchData(spell) {
 }
 
 // fetchData();
+
 //Showing the Spell information
-//Showing the name of spell
 function showSpellData(data) {
-  const spellName = document.createElement("h2");
+  const spellName = document.createElement("h2"); //Showing the name of spell
   spellName.innerText = `${data.name}`;
   divLeftPage.appendChild(spellName);
 
-  //Showing Casting time
-  const castingTime = document.createElement("p");
+  const castingTime = document.createElement("p");   //Showing Casting time
   castingTime.innerText = `Casting Time: \n${data.casting_time}`;
   divLeftPage.appendChild(castingTime);
 
@@ -40,30 +39,50 @@ function showSpellData(data) {
   concentration.innerText = `Concentration:\n${data.concentration}`;
   divLeftPage.appendChild(concentration);
 
-  //Showing Saving Throw Info
-  const savingThrow = document.createElement("p")
-  savingThrow.innerText = `Saving Throw:\n${data.dc.dc_type.name}`
-  divLeftPage.appendChild(savingThrow);
-  // else {
-  //   const savingThrow = document.createElement("p");
-  //   savingThrow.innerText = "There is no Saving Throw";
-  //   divLeftPage.appendChild(savingThrow);
-  // };
-
-  //Showing Damage Type
-  const damageType = document.createElement("h2");
-  damageType.innerText = `Damage Type: \n${data.damage.damage_type.name}`
-  divRightPage.appendChild(damageType);
-
-  //Showing Damage per Spell Slot
-  const damage = document.createElement("h2");
-  damage.innerText = `Damage: \n${data.damage.damage_at_slot_level}`;
-  divRightPage.appendChild(damage)
-
+  // Showing Description of Spell
   const description = document.createElement("p");
   description.innerText = `${data.desc}`;
   divRightPage.appendChild(description);
 }
+
+function spellDamageOrHealing(data) {
+  const spellData = response.data;
+  if (spellData.includes(heal_at_slot_level)) {
+    const healData = document.createElement("p");
+    healData = data.heal_at_slot_level[1];
+    divRightPage.appendChild(healData)
+  }
+  else if (spellData.includes(damage_at_slot_level)) {
+    const damage = document.createElement("h2");   //Showing Damage per Spell Slot
+    damage.innerText = `Damage: \n${data.damage.damage_at_slot_level}`;
+    divRightPage.appendChild(damage)
+
+    const damageType = document.createElement("h2");   //Showing Damage Type
+    damageType.innerText = `Damage Type: \n${data.damage.damage_type.name}`
+    divRightPage.appendChild(damageType);
+
+  }
+}
+
+
+
+
+
+
+
+//Showing Saving Throw Info
+const savingThrow = document.createElement("p")
+savingThrow.innerText = `Saving Throw:\n${data.dc.dc_type.name}`
+divLeftPage.appendChild(savingThrow);
+// else {
+//   const savingThrow = document.createElement("p");
+//   savingThrow.innerText = "There is no Saving Throw";
+//   divLeftPage.appendChild(savingThrow);
+// };
+
+
+
+
 
 // Search functionality
 searchButton.addEventListener("click", handleInput);
